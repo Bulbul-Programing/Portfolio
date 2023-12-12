868f8f108494d83c2ef69fee1924336a31e092cc
Bulbul-Programing <bulbul.programing@gmail.com>
2023-12-12 19:06:55 +0600

    add fotter section.

--- src/Home/LatestProject/LatestProject.jsx
+++ src/Home/LatestProject/LatestProject.jsx
@@ -13,23 +13,23 @@ const LatestProject = () => {
                         <h1 className="text-3xl font-bold my-5 md:my-0 lg:my-0">Turbo Trade</h1>
                         <p className="text-slate-500 my-3 lg:w-3/4">TurboTrade: Your go-to online hub for buying and selling cars. Experience a sleek platform, connecting car enthusiasts for seamless transactions. Drive your passion with TurboTrade.</p>
                     </div>
-                    <NavLink to='https://turbotrade-753dc.web.app/'><button className="font-bold my-5 btn text-white bg-blue-400 hover:text-black">Explore</button></NavLink>
+                    <NavLink to='https://turbotrade-753dc.web.app/' target="_blank"><button className="font-bold my-5 btn text-white bg-blue-400 hover:text-black">Explore</button></NavLink>
                 </div>
                 <div className="flex flex-col mb-7 md:flex-row lg:flex-row items-center md:gap-x-4 lg:gap-x-10 shadow-xl p-5 md:p-4 lg:p-7 rounded-lg">
                     <img className="md:w-[200px] lg:w-[300px] rounded-lg" src="https://i.ibb.co/qB7m9GD/career-vista.png" alt="" />
                     <div>
                         <h1 className="text-3xl font-bold my-5 md:my-0 lg:my-0">Career Vista</h1>
-                        <p className="text-slate-500 my-3 lg:w-3/4">TurboTrade: Your go-to online hub for buying and selling cars. Experience a sleek platform, connecting car enthusiasts for seamless transactions. Drive your passion with TurboTrade.</p>
+                        <p className="text-slate-500 my-3 lg:w-3/4">Revolutionize hiring and job seeking on our platform. Streamlined job posting and application process, connecting employers with top talent and candidates with their dream opportunities effortlessly.</p>
                     </div>
-                    <NavLink to='https://turbotrade-753dc.web.app/'><button className="font-bold my-5 btn text-white bg-blue-400 hover:text-black">Explore</button></NavLink>
+                    <NavLink to='https://career-vista.firebaseapp.com/' target="_blank"><button className="font-bold my-5 btn text-white bg-blue-400 hover:text-black">Explore</button></NavLink>
                 </div>
                 <div className="flex flex-col md:flex-row lg:flex-row items-center md:gap-x-4 lg:gap-x-10 shadow-xl p-5 md:p-4 lg:p-7 rounded-lg">
                     <img className="md:w-[200px] lg:w-[300px] rounded-lg" src="https://i.ibb.co/bP5k4zF/survey-pluse.png" alt="" />
                     <div>
                         <h1 className="text-3xl font-bold my-5 md:my-0 lg:my-0">Survey Pluse</h1>
-                        <p className="text-slate-500 my-3 lg:w-3/4">TurboTrade: Your go-to online hub for buying and selling cars. Experience a sleek platform, connecting car enthusiasts for seamless transactions. Drive your passion with TurboTrade.</p>
+                        <p className="text-slate-500 my-3 lg:w-3/4">Empower opinions with our user-friendly survey website. Seamlessly design, distribute, and analyze surveys for valuable insights. Transform data into actionable results effortlessly.</p>
                     </div>
-                    <NavLink to='https://turbotrade-753dc.web.app/'><button className="font-bold my-5 btn text-white bg-blue-400 hover:text-black">Explore</button></NavLink>
+                    <NavLink to='https://survey-plus-18f6b.firebaseapp.com/' target="_blank"><button className="font-bold my-5 btn text-white bg-blue-400 hover:text-black">Explore</button></NavLink>
                 </div>
             </div>
         </div>
