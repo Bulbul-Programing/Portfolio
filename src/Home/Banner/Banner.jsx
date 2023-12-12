b73bae1237cfeb4af00d8f6fd2a152a0f0e02239
Bulbul-Programing <bulbul.programing@gmail.com>
2023-12-12 18:34:51 +0600

    download link add

--- src/Home/Banner/Banner.jsx
+++ src/Home/Banner/Banner.jsx
@@ -1,5 +1,6 @@
 import './banner.css'
 import { FaDownload } from "react-icons/fa";
+import Portfolio from '../../../public/Portfolio.pdf'
 
 const Banner = () => {
     return (
@@ -9,7 +10,9 @@ const Banner = () => {
                 <h1 className='font-bold my-3 text-lg hover:text-blue-500 hover:duration-150'>Junior MERN Stack Developer</h1>
                 <p className='w-3/4 text-slate-600'>Eager learner and MERN stack practitioner, dedicated to honing skills in MongoDB, Express.js, React, and Node.js, with a focus on building robust and user-friendly web applications.</p>
                 <div>
-                    <button className='btn bg-blue-400 text-white hover:text-black font-medium my-5'>DownLoad Cv <FaDownload></FaDownload></button>
+                    <a href={Portfolio} download='Portfolio.pdf' rel="noreferrer" target={'_blank'}>
+                        <button className='btn bg-blue-400 text-white hover:text-black font-medium my-5'>DownLoad Cv <FaDownload></FaDownload></button>
+                    </a>
                 </div>
             </div>
             <div className='md:w-2/4 lg:w-2/6'>
